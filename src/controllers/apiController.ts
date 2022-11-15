552e795a7c0f18a1efe6b32d5ad36ebb356ecd66
ernane <ernanecarvalho89@gmai.com>
2022-11-15 10:41:57 -0300

    apiController

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -19,5 +19,21 @@ export const createPhrase = async (req: Request, res: Response) => {
     let newPhrase = await Phrase.create({
         author, txt
     })
+    res.status(201);
     res.json({id: newPhrase.id, author, txt});
+}
+
+export const listPhrases = async (req: Request, res: Response) => {
+    let list = await Phrase.findAll();
+    res.json({list});
+}
+
+export const getPhrase = async (req: Request, res: Response) => {
+    let {id} = req.params;
+    let phrase = await Phrase.findByPk(id);
+    if(phrase){
+        res.json({phrase})
+    }else{
+        res.json({error: 'Frase nao encontrada.'});
+    }
 }
